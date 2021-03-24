c160dabc609dde011596e4ddaf823549e14748bc
krittamet chanphet <devil040642@gmail.com>
2021-03-24 14:54:50 +0700

    merge boss+tle file

--- src/navigations/Navigator.js
+++ src/navigations/Navigator.js
@@ -1,23 +1,25 @@
-import {createStackNavigator} from 'react-navigation-stack'
-import {createAppContainer} from 'react-navigation'
+import { createStackNavigator } from 'react-navigation-stack'
+import { createAppContainer } from 'react-navigation'
 import Login from '../screens/Login'
 import Home from '../screens/Home'
 import Detail from '../screens/Detail'
 import { AppLoading } from 'expo'
 import Landing from '../screens/Landing'
+import Profile from '../screens/Profile'
 
 const stackNavigatorOptions = {
-    headerShown :false
+    headerShown: false
 }
 
 const AppNavigator = createStackNavigator({
-    Landing:{screen:Landing},
-    Login:{screen:Login},
-    Home:{screen:Home},
-    Detail:{screen:Detail}
+    Landing: { screen: Landing },
+    Login: { screen: Login },
+    Home: { screen: Home },
+    Detail: { screen: Detail },
+    Profile: { screen: Profile }
 },
     {
-        defaultNavigationOptions:stackNavigatorOptions
+        defaultNavigationOptions: stackNavigatorOptions
     }
 )
 export default createAppContainer(AppNavigator);
